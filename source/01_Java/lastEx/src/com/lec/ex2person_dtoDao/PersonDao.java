2d82d2bd59a2de00b3d4606ef39b97bfdf7ab52d
Jeonghoike <jhi6334@naver.com>
2023-08-08 18:03:39 +0900

    JDBC

--- source/01_Java/lastEx/src/com/lec/ex2person_dtoDao/PersonDao.java
+++ source/01_Java/lastEx/src/com/lec/ex2person_dtoDao/PersonDao.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class PersonDao {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
-	private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	private String url    = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 	public final static int SUCCESS = 1;
 	public final static int FAIL    = 0;
 	private static PersonDao INSTANCE = new PersonDao();
